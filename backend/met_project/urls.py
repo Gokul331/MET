316ff80c1911f476b742457f04fc8a41100411b3
Gokul <gokul363@Gokuls-MacBook-Air.local>
2026-07-12 23:41:10 +0530

    fix: serve media files in production

--- backend/met_project/urls.py
+++ backend/met_project/urls.py
@@ -24,4 +24,15 @@ urlpatterns = [
     path('api/', include('colleges.urls')),
     path('api/', include('applications.urls')),
     path('api/', include('contact.urls')),
-] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+]
+
+if not settings.DEBUG:
+    from django.urls import re_path
+    from django.views.static import serve
+    urlpatterns += [
+        re_path(r'^media/(?P<path>.*)$', serve, {
+            'document_root': settings.MEDIA_ROOT,
+        }),
+    ]
+else:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
